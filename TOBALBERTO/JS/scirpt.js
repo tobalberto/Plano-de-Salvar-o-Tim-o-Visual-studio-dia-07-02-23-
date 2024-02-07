706471a557066c299fbc29a5f0fc074497c55c87
TOBALBERTO <159040994+tobalberto@users.noreply.github.com>
2024-02-07 09:23:18 -0400

    Add files via upload

---
+++ TOBALBERTO/JS/scirpt.js
@@ -0,0 +1,15 @@
+function validaCampos(){
+
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+    
+    if(usuario != "Cassio Ramos" && senha != "sccp1910"){
+        alert("Usuário ou senha invalidos")
+    }else{
+        alert("Seja Bem vindo a Reconstrução do Timão".usuario);
+    }
+    
+      
+
+    alert(usuario);
+}
